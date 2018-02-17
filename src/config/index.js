0cca1fffe211f8186e1575312054d0862455b4cd
Neeraj <n.sharma1396@gmail.com>
2018-02-17 12:03:08 +0530

    first commit

---
+++ src/config/index.js
@@ -0,0 +1,9 @@
+  // Initialize Firebase
+  export const DB_CONFIG = {
+    apiKey: "AIzaSyDw5IwCQT7UFAkYmNMecyqkiAxlDGmxlAA",
+    authDomain: "contacts-filler.firebaseapp.com",
+    databaseURL: "https://contacts-filler.firebaseio.com",
+    projectId: "contacts-filler",
+    storageBucket: "contacts-filler.appspot.com",
+    messagingSenderId: "560173941660"
+  };
